12a42990656655b527fcea2604bb1745547da919
Luis1306-jc <jimenezl1306@gmail.com>
2026-02-25 12:47:46 -0600

    actualizacion en niveles

--- Scripts/game_over.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-func _on_reintentar_pressed():
-	Global.moneda = 0
-	Global.llave = false
-	get_tree().change_scene_to_file("res://Escenas/game.tscn")
-
-func _on_menu_pressed():
-	get_tree().change_scene_to_file("res://Escenas/Menu_Principal.tscn")
