2524e379e5018942aee8c8e4fd26b9e6ae926622
betower24 <127764841+betower24@users.noreply.github.com>
2026-03-11 11:22:09 -0600

    ya funciona nivel 4y5
    pto el que lo lea

--- Scripts/puerta.gd
+++ Scripts/puerta.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@export var siguiente_nivel: String = "res://Escenas/nivel_2.tscn"
+@export var siguiente_nivel: String = "res://Escenas/menu_principal.gd"
 
 func _on_body_entered(body):
 	if body.is_in_group("player") and Global.llave:
