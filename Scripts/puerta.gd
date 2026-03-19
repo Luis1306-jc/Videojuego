ed096385e7a83ed8266ddf3f0f7ba372eba1a8ad
Luis1306-jc <jimenezl1306@gmail.com>
2026-03-18 21:40:34 -0600

    cambios nuevos en los niveles

--- Scripts/puerta.gd
+++ Scripts/puerta.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@export var siguiente_nivel: String = "res://Escenas/mapas.tscn"
+@export var siguiente_nivel: String = "res://Escenas/nivel_2.tscn"
 
 func _on_body_entered(body):
 	if body.is_in_group("player") and Global.llave:
